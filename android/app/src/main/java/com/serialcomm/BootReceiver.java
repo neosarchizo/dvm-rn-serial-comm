85044786a604e54fe080dfbdae804842aeb49782
neosarchizo <neosarchizo@gmail.com>
2022-03-21 15:21:37 +0900

    feat: update android

---
+++ android/app/src/main/java/com/serialcomm/BootReceiver.java
@@ -0,0 +1,14 @@
+package com.serialcomm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    Intent i = new Intent(context, MainActivity.class);
+    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    context.startActivity(i);
+  }
+}
--- android/app/src/main/java/com/serialcomm/MainActivity.java
+++ android/app/src/main/java/com/serialcomm/MainActivity.java
@@ -1,5 +1,10 @@
 package com.serialcomm;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +16,34 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "SerialComm";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    hideNavigationBar();
+  }
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+      hideNavigationBar();
+    }
+  }
+
+  private void hideNavigationBar() {
+    final int flags =
+      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+       | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+       | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+       | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+       | View.SYSTEM_UI_FLAG_FULLSCREEN
+       | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+    getWindow().getDecorView().setSystemUiVisibility(flags);
+    getWindow().setStatusBarColor(Color.TRANSPARENT);
   }
 }
